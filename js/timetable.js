99369f8fdfbc509a9e89b4a7ae030d987aa27e65
leberechttask <leberechttask@gmail.com>
2017-11-02 16:16:39 +0900

    Update Uchipro timetable(and update index.html)

--- js/timetable.js
+++ js/timetable.js
@@ -6,219 +6,219 @@ var timetable = {
                 "id": 1,
                 "hour": 10,
                 "start_minute": 00,
-                "end_minute": 20,
+                "end_minute": 29,
                 "name": "OP ~1日目~",
                 "description": "THK2016ラジオパレードスタートです！！"
             },
             {
                 "id": 2,
                 "hour": 10,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "乃木坂ラジオ",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "課題DJ4",
                 "description": "乃木坂46を愛してやまない2人がラジオパレードに登場！彼らのラジオを聴いた瞬間、君も立派な乃木坂ファン！？NMT(なぁちゃん、マジ、天使！)"
             },
             {
                 "id": 3,
-                "hour": 10,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "あなたの知らないゆずを語るラジオ",
+                "hour": 11,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "仮面ライダーITF.",
                 "description": "「ゆず」と聞いて柑橘系のあの果物を思い浮かべた方…だけではなく、「栄光の架橋」を思い浮かべたそこのあなたにも届けます。「感動」のみならない「ゆず」をご堪能あれ！"
             },
             {
                 "id": 4,
                 "hour": 11,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "男ラジヲ",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "外部DJ（Sheep）",
                 "description": "日常で起こる多数のイベント。漢らしく振舞うとはどういうことか。漢とは何か。それは永遠の謎。"
             },
             {
                 "id": 5,
                 "hour": 11,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "課題DJ3",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "ディズニーリゾートラジオ",
                 "description": "新入生によるフレッシュなラジオをお楽しみください"
             },
             {
                 "id": 6,
-                "hour": 11,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "アイナナラジオ",
+                "hour": 12,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "女子校ラジオ2017",
                 "description": "｢アイナナ｣ことアイドリッシュセブンの魅力を、アイナナ大好きパーソナリティの2人が熱く語ります。技術の2人の動きにもご注目ください！アイナナはいいぞ……！"
             },
             {
                 "id": 7,
                 "hour": 12,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "陸同ラジオ",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "課題DJ3",
                 "description": "陸上同好会のひとが陸上に関するあれこれを話してくれます。普段とは少し違ったラジオをお楽しみください"
             },
             {
                 "id": 8,
                 "hour": 12,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "特別ゲスト:ゆいまつり",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "THK縦女子会",
                 "description": "今年のゲストは、毎年3月につくばゆかりの食やパフォーマンスが集うイベントを運営する「ふるさとつくば ゆいまつり」さん！つくば愛に溢れたラジオをお楽しみに！"
             },
             {
                 "id": 9,
-                "hour": 12,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "シャッフル1",
+                "hour": 13,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "比較ラジオ学類",
                 "description": "ランダムで集められたメンバーによる予測のつかないラジオをお楽しみください"
             },
             {
                 "id": 10,
                 "hour": 13,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "特別ゲスト:VICC",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "外部DJ1（Guest: Viccさま）",
                 "description": "ViCC-映画をつくる会-さんをゲストに迎え、学園祭での出展の話や日々の活動について詳しくお伺いしていきます！！"
             },
             {
                 "id": 11,
                 "hour": 13,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "がむりかべーラジオ第352回",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "とりかごラジオ",
                 "description": "352回目となる今回は、夏休みにディズニーシーに行った3人のアトラクション体験記をお伝えします！ディズニーマスターの里佳先生から裏話も聞けるかも…！？"
             },
             {
                 "id": 12,
-                "hour": 13,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "拝啓、ゆとり君へ",
+                "hour": 14,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "スマブラジオ",
                 "description": "今年の春に放送された宮藤官九郎さん脚本のドラマ「ゆとりですがなにか」を語ります。自分ゆとり世代だと思ったそこのあなた、ちょっと寄っていきませんか？"
             },
             {
                 "id": 13,
                 "hour": 14,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "井上・真央のワンダーラジオ",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "Radio on Ice 2017",
                 "description": "みんなに夢と希望をお届け！これを聞けばあなたも今日から紳士淑女になれるかも？大人のための大人のラジオ！"
             },
             {
                 "id": 14,
                 "hour": 14,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "コブクロラジオ",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "限界（）レディオ",
                 "description": "「自分、歌上手いな」「自分もめっちゃ、うまいなぁ」堺の路上で偶然出会った2人。それが今となっては音楽シーンを引っ張る活躍を見せる2人のことをわずかですが語らせていただきます。"
             },
             {
                 "id": 15,
-                "hour": 14,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "イブニング1",
+                "hour": 15,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "女児の女児による女児のためのラジオ",
                 "description": "夕方のひととき、ラジオを聞いていきませんか？"
             },
             {
                 "id": 16,
                 "hour": 15,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "闇の晩餐会",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "DJタイム",
                 "description": "時は満ちた。夢幻の眠りから覚めた我々を止めることは容易でない。ならば受け入れろ、闇を、己を、生を。覇を認め、見果てぬ夢の結末を知るために、共に逝こうではないか"
             },
             {
                 "id": 17,
                 "hour": 15,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "埼玉の本気",
-                "description": "2016年都道府県魅力度ランキング、埼玉県はついに40位を脱します。期は熟し、来たる決戦のとき、果たして埼玉県は東京都を負かすことができるのか…！"
-            },
-            {
-                "id": 18,
-                "hour": 15,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "仮面ライダーITF",
+                "name": "コミュニカっていう会誌で2年連続ベストコンビに選ばれた僕らだけど実際のところはどうなんだろう？ラジオ",
+v                "description": "2016年都道府県魅力度ランキング、埼玉県はついに40位を脱します。期は熟し、来たる決戦のとき、果たして埼玉県は東京都を負かすことができるのか…！"
+ f           },
+            {
+                "id": 18,
+                "hour": 16,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "Gトーク",
                 "description": "マイティアクションエックス！レッツゲーム！メッチャゲーム！ムッチャゲーム！ワッチャネーム！We are KAMENRIDERs！ラジオを聞けば君も仮面ライダー！"
             },
             {
                 "id": 19,
                 "hour": 16,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "引退番発ってなぁに？",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "俺がお前で、お前が俺で",
                 "description": "もう秋ですね。引退番発の時期になってきましたね。THK渉外部員の端くれが番組発表会についてお話しますね。"
             },
             {
                 "id": 20,
                 "hour": 16,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "うみべの町中ラジオ",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "みんくさん！何が聞きたい！？",
                 "description": "なんとなく集められた4人。彼らの唯一の共通点は『誰よりも面白いことが言えるという自負』。勝手に学年代表となった彼らは、学年の維持と誇りを賭けた闘いに挑む…！！"
             },
             {
                 "id": 21,
-                "hour": 16,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "シャッフル2",
+                "hour": 17,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "みんなのうた",
                 "description": "ランダムで集められたメンバーによる予測のつかないラジオをお楽しみください"
             },
             {
                 "id": 22,
                 "hour": 17,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "エリクサーを中盤のボスで使ってしまうけど友達の前では「エリクサーって最後まで残るよな（笑）」と言う人たち",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "ぷえ☆らじ",
                 "description": "今、エリクサー、使ったろ？"
             },
             {
                 "id": 23,
                 "hour": 17,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "田中のぼっちラジオ",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "女子高ラジオ2017",
                 "description": "友達の少ない田中が雙峰祭の場を借りてお友達を募集します！田中が独り、「数打ちゃ当たる」方式で趣味を垂れ流す15分。なお、技術はおのまちこでお送りします。"
             },
             {
                 "id": 24,
-                "hour": 17,
-                "start_minute": 40,
-                "end_minute": 59,
-                "name": "課題DJ1",
+                "hour": 18,
+                "start_minute": 00,
+                "end_minute": 19,
+                "name": "課題DJ2",
                 "description": "新入生によるフレッシュなラジオをお楽しみください"
             },
             {
                 "id": 25,
                 "hour": 18,
-                "start_minute": 00,
-                "end_minute": 20,
-                "name": "ﾐﾐﾐの部屋 ＼ﾌｧｲﾅﾙ／",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "らふあんどぴーす",
                 "description": "ﾐｰﾐﾐﾐﾐﾐﾐｰﾐﾐﾐﾐﾐ……高らかな笑いと華麗なツッコミが響き渡る『 ﾐﾐﾐの部屋 ＼ﾌｧｲﾅﾙ／ 』2年間の終止符をうちます！来てね！！！！！"
             },
             {
                 "id": 26,
                 "hour": 18,
-                "start_minute": 20,
-                "end_minute": 40,
-                "name": "下剋上～最終章～",
+                "start_minute": 40,
+                "end_minute": 59,
+                "name": "外部DJ（Guest: 斬桐舞さん）",
                 "description": "負けられない戦いがそこにある。圧倒的弱者が圧倒的強者に立ち向かう！これが本当の下克上！"
             },
             {
                 "id": 27,
-                "hour": 18,
-                "start_minute": 40,
+                "hour": 19,
+                "start_minute": 00,
                 "end_minute": 59,
-                "name": "元男子高校生の日常",
+                "name": "THKラジオ学類！",
                 "description": "通っていた男子校に思いを馳せ、この世の中の常識を疑い、いかに安全なトークが出来るのか。我々は明日もラジオが出来るのか。試される15分。男子校万歳。"
             },
-            {
+/*            {
                 "id": 28,
                 "hour": 19,
                 "start_minute": 00,
@@ -226,22 +226,23 @@ var timetable = {
                 "name": "THKラジオ学類!",
                 "description": "1日目のトリはラジ学生収録！お祭りの雰囲気を感じながらパーの2人が最後を締めくくります！「THKラジオ学類！」は、fm84.2MHzより毎週日曜22時30分放送！"
             },
+*/
         ],
         "day2": [
             {
                 "id": 1,
                 "hour": 10,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "OP~2日目~",
+                "end_minute": 19,
+                "name": "会長の捨て台詞",
                 "description": "雙峰祭２日目ですみなさん、準備はいいですか？"
             },
             {
                 "id": 2,
                 "hour": 10,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "なかよくなりました(仮)",
+                "end_minute": 39,
+                "name": "内プロラジオ",
                 "description": "2年前、なかよくなるためにDJトークをした2人が「なかよくなりました」と称し再びトークに挑みます…ってあれ、タイトルの（仮）はどういうことだ！！"
             },
             {
@@ -249,23 +250,23 @@ var timetable = {
                 "hour": 10,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "柿ピーラジオ",
+                "name": "福岡弁いいなぁ、とよく言われるけど、その実態を確かめてもらうラジオ",
                 "description": "みんな大好き柿ぴー。しかしそこには柿ピー派、ピーナッツ派、バランス派という３つの宗派が存在した…柿ピーのプライドを賭けた戦いが今始まる…"
             },
             {
                 "id": 4,
                 "hour": 11,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "特別ゲスト:斬桐舞",
+                "end_minute": 19,
+                "name": "サイコロラジオ",
                 "description": "筑波大学　斬桐舞さんとお送りする『きりきりの時間☆』！「あなたが知ってるソーランだけがソーランじゃない？！」午後のステージが見逃せなくなる15分間。お楽しみに！"
             },
             {
                 "id": 5,
                 "hour": 11,
-                "start_minute": 00,
-                "end_minute": 40,
-                "name": "クリープハイプを語るラジオ",
+                "start_minute": 20,
+                "end_minute": 39,
+                "name": "ITF.のらーめん好きが語るらーめんらぢお",
                 "description": "回りくどくて単純で、切なくて痛くて、でも時々温かい。そんなクリープハイプの世界観に魅了された3人が、好きな曲について思う存分語ります。"
             },
             {
@@ -273,23 +274,23 @@ var timetable = {
                 "hour": 11,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "課題DJ2",
+                "name": "ゲス極のゲスくない世界",
                 "description": "新入生のフレッシュなラジオをお楽しみください"
             },
             {
                 "id": 7,
                 "hour": 12,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "大森靖子の女の子世界",
+                "end_minute": 19,
+                "name": "アイマスラジオ",
                 "description": "シンガーソングライターの大森靖子さんが描く独特で平凡な世界。彼女が紡ぎ出す言葉はあなたを救うかもしれない。彼女の歌詞を勝手に1人で語ります。"
             },
             {
                 "id": 8,
                 "hour": 12,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "精神力を鍛えるラジオ",
+                "end_minute": 39,
+                "name": "ピンドラジオ！",
                 "description": "パーソナリティの2人が己の精神力を鍛えるべく、世の中のネガティブな出来事をひたすらポジティブにとらえていく、そんなラジオです。届け元気、鍛えろ精神！！"
             },
             {
@@ -297,23 +298,23 @@ var timetable = {
                 "hour": 12,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "東葛ライフ！",
+                "name": "この世の地獄ラジオ",
                 "description": "千葉県立東葛飾高校の卒業生がお送りする生ラジオ！東葛に縁のある方もない方も楽しめる内容となっておりますのでご安心ください（？）"
             },
             {
                 "id": 10,
                 "hour": 13,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "イブニング2",
+                "end_minute": 19,
+                "name": "おきなみ55",
                 "description": "夕方のひととき、ラジオを聞いていきませんか？"
             },
             {
                 "id": 11,
                 "hour": 13,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "課題DJ5",
+                "end_minute": 39,
+                "name": "目指せ！フル単",
                 "description": "新入生によるフレッシュなラジオをお楽しみください"
             },
             {
@@ -321,23 +322,23 @@ var timetable = {
                 "hour": 13,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "アニ担ラジオ",
+                "name": "一人語りっかり〜ラジオ",
                 "description": "この番組では、事前に行ったアンケートを元にアナウンス局担当代の4人がアニメについて語り、妄想します！アニメ好きの4人がどんなトークを繰り広げるのかお楽しみに！"
             },
             {
                 "id": 13,
                 "hour": 14,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "限界レディオ",
+                "end_minute": 19,
+                "name": "でこぼころっく",
                 "description": "「我々は限界の呪縛から解き放たれることはできない。」限界を超えた先に見えたものとは？このレディオを聞いた後、あなたは思わず発してしまうだろう、その単語は…、"
             },
             {
                 "id": 14,
                 "hour": 14,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "にちにち！",
+                "end_minute": 39,
+                "name": "中国VS四国ラジオ",
                 "description": "今年もやります、「にちにち！」ラジオ。今回はTHKのにちにちメンバーから１・２・３年生が集結し、学年対抗で日本語力対決を行います！果たして優勝は誰の手に…！？"
             },
             {
@@ -345,23 +346,23 @@ var timetable = {
                 "hour": 14,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "むちゃぶりラジオ",
+                "name": "はとくまラジオ",
                 "description": "THK一年の二人が、構成さんからの「愛のある」むちゃぶりに答えて一周り成長したTHK会員を目指します。フレッシュな一年生の成長過程をやさしく見守ってください！！"
             },
             {
                 "id": 16,
                 "hour": 15,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "ゆるらじ",
+                "end_minute": 19,
+                "name": "課題DJ5",
                 "description": "ゆるっとした3人でトークを繰り広げていく「ゆるラジ」。本番組中の企画では身近に潜む様々な雑学についてクイズ形式で出題します。パーソナリティの解答にも注目！"
             },
             {
                 "id": 17,
                 "hour": 15,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "ゆめりかみん",
+                "end_minute": 39,
+                "name": "ゆっきーのひとりしゃべり",
                 "description": "THKゆるふわ女子ゆめ・りか・みみみによるラジオ―――「ゆめりかみん」！！！3人が織りなすゆるふわトークをぜひ目と耳と頭を使いながらご堪能ください♡♡"
             },
             {
@@ -369,23 +370,23 @@ var timetable = {
                 "hour": 15,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "シャッフル3",
+                "name": "THKぬいぐるみ部！",
                 "description": "ランダムで集められたメンバーによる予測のつかないラジオをお楽しみください"
             },
             {
                 "id": 19,
                 "hour": 16,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "課題DJ4",
+                "end_minute": 19,
+                "name": "課題DJ1",
                 "description": "新入生によるフレッシュなラジオをお楽しみください"
             },
             {
                 "id": 20,
                 "hour": 16,
                 "start_minute": 20,
-                "end_minute": 40,
-                "name": "でこぼころっく！",
+                "end_minute": 39,
+                "name": "立つ鳥跡を濁す",
                 "description": "身長伸ばしたい二人…でも大学生だし伸びない…あっ、そうだ！THK会員の〇〇を使って伸ばそう！なラジオです。"
             },
             {
@@ -393,17 +394,34 @@ var timetable = {
                 "hour": 16,
                 "start_minute": 40,
                 "end_minute": 59,
-                "name": "ラジオ制作局老人会!",
+                "name": "じゅじゅくんとあそぼう！",
                 "description": "引退間近のラジオ制作局3年生でお送りする番組です。1、2年生に比べるとパワーでは勝てないかもしれませんが…聴かせます、3年の本気。"
             },
             {
                 "id": 22,
                 "hour": 17,
                 "start_minute": 00,
-                "end_minute": 20,
-                "name": "ED",
+                "end_minute": 19,
+                "name": "dan・de・marian The Last Dance",
                 "description": "THK2016ラジオパレードスタート楽しんでいただけましたか。最後のひととき、ラジオはいかがですか？"
             },
+						{
+							"id": 23,
+							"hour": 17,
+							"start_minute": 20,
+							"end_minute": 39,
+							"name": "ラジ執の本気",
+							"description": ""
+						},
+						{
+							"id": 23,
+							"hour": 17,
+							"start_minute": 40,
+							"end_minute": 59,
+							"name": "エンディング",
+							"description": ""
+						},
+						}
         ],
     },
     /*シアターは幕の順番と幕の中身を別で作成*/ 
